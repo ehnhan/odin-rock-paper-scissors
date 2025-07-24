2d5f87b1c44247222e107785edb47c50093fcf34
Eric <e.h.nhan@gmail.com>
2025-07-24 15:01:35 -0700

    Create RPS with buttons

--- script.js
+++ script.js
@@ -14,38 +14,58 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Enter your choice:");
-}
-
-
 function playRound(human, computer) {
     human = human.toLowerCase()
     computer = computer.toLowerCase()
 
     if (human === computer) {
         console.log("Tie")
+        currentResult.textContent = "Tie"
     } else if ((human === "rock" && computer == "scissors") ||
         (human === "scissors" && computer == "paper") ||
         (human === "paper" && computer == "rock")) 
     { 
         console.log("Winner");
+        currentResult.textContent = "Winner"
+
         humanScore++;
     } else {
         console.log("You Lost");
+        currentResult.textContent = "Lost"
+
         computerScore++;
     }
+
+    runningScore.textContent = `Human: ${humanScore} vs Computer: ${computerScore}`;
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    if (humanScore > computerScore) {
-        console.log("You won the game!");
-    } else {
-        console.log("You lost the game.")
-    }
-}
+const results = document.querySelector(".results");
+const currentResult = document.querySelector(".currentResult");
+const runningScore = document.querySelector(".runningScore");
 
-playGame();
+const buttonsDiv = document.querySelector(".optionButtons");
+buttonsDiv.addEventListener("click", (e) => {
+    const isButton = e.target.nodeName === 'BUTTON';
+    if (isButton) {
+        let humanChoice = e.target.textContent;
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+
+        if (humanScore >= 5) {
+            console.log("You won the game!");
+            currentResult.textContent = "You won the game!"
+            endGame()
+        } else if (computerScore >= 5) {
+            console.log("You lost the game.");
+            currentResult.textContent = "You lost the game."
+            endGame()
+        }
+    }
+});
+
+function endGame() {
+    const buttons = document.querySelectorAll('.optionButtons button');
+    buttons.forEach(button => {
+        button.disabled = true; // Disable each button
+    });
+}
